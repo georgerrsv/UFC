fe33730bb34d433f8d96e2a30688334d4dc5e281
georgerrs <georgerrsilva@alu.ufc.br>
2023-09-08 12:12:54 -0300

    update

--- scripts/atividade04/script.sh
+++ scripts/atividade04/script.sh
@@ -7,7 +7,7 @@ chmod 0700 soma.sh saudacao.sh
 
 echo -e "Playstation5 4999\nMacBook 9799\nGalaxyS20Ultra 5499\niPad 3499\nSmartTV554k 2799" > compras.txt
 
-echo 'cat compras.txt | cut -d " " -f2 | tr '\n' '+' | sed 's/+$/\n/' | bc' > soma.sh
+echo 'cat compras.txt | cut -d " " -f2 | tr "\n" "+" | sed "s/+$/\n/" | bc' > soma.sh
 
 echo '#!/bin/bash
 export TZ=America/Sao_Paulo
