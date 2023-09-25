fa20fc31d0c415a54916f0fe4b1c87c5bb665c3e
georgerrs <georgerrsilva@alu.ufc.br>
2023-09-25 12:14:31 -0300

    update

--- scripts/atividade06/cinco_diretorios.sh
+++ scripts/atividade06/cinco_diretorios.sh
@@ -2,25 +2,24 @@
 
 mkdir -p cinco
 
-for ((i=1; i<=5; i++)); 
-    do
-        mkdir -p cinco/dir$i
+for ((i=1; i<=5; i++)); do
+    mkdir -p cinco/dir$i
 
-        for ((j=1; j<=4; j++)); 
-            do
-                conteudo=""
+    for ((j=1; j<=4; j++)); do
 
-        for ((k=1; k<=j; k++)); 
-            do
-                conteudo="$conteudo$j"
+        CONTEUDO=""
 
-                if [ "$k" -lt "$j" ]; 
-                    then
-                    
-                        conteudo="$conteudo"$'\n'
-                fi
-            done
+        for ((k=1; k<=j; k++)); do
 
-            echo -e "$conteudo" > cinco/dir$i/arq$j.txt
+            CONTEUDO="$CONTEUDO$j"
+            
+            if [ "$k" -lt "$j" ]; 
+                then
+                    CONTEUDO="$CONTEUDO"$'\n'
+            fi
+            
         done
+
+        echo -n -e "$CONTEUDO" > cinco/dir$i/arq$j.txt
     done
+done
