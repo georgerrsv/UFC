1906c55aff7d9613a87ff3ec66574f1714c90c95
George <georgerrsilva@alu.ufc.br>
2023-10-29 04:20:34 -0300

    update

--- sd/trabalhoFinal/new/proxy.py
+++ sd/trabalhoFinal/new/proxy.py
@@ -19,7 +19,7 @@ class Proxy:
 
         filme = Filme(titulo, diretor, ano, duracao, genero, classificacao, descricao)
         filme_json = filme.to_json()
-        cabecalho = Cabecalho(0, "cadastrar", 1, {"filme": filme_json})
+        cabecalho = Cabecalho(0, "adicionarFilme", 1, {"filme": filme_json})
         h_json = cabecalho.to_json()
         self.client.sendRequest(h_json)
         data = self.client.getResponse()
@@ -29,7 +29,7 @@ class Proxy:
     def removerFilme(self):
         print("[Remover]")
         id = int(input("ID: "))
-        cabecalho = Cabecalho(0, "remover", 2, {"id": id})
+        cabecalho = Cabecalho(0, "removerFilme", 2, {"id": id})
         h_json = cabecalho.to_json()
         self.client.sendRequest(h_json)
         data = self.client.getResponse()
@@ -55,7 +55,9 @@ class Proxy:
         data = self.client.getResponse()
         if data:
             header = Cabecalho.from_json(data)
-            catalogo = loads(header.arguments)
+            catalogo = header.arguments
             for filme_data in catalogo:
                 filme = Filme.from_json(filme_data)
                 print(filme.to_json())
+        else:
+            print("Não foi possível obter uma resposta do servidor.")
